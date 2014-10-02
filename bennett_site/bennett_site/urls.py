28823be87d82a166c3249e07d7ad919fa81ec416
Will Tachau <wtachau@gmail.com>
2014-10-02 16:19:03 -0700

    site is up

--- bennett_site/bennett_site/urls.py
+++ bennett_site/bennett_site/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from home_page.views import *
 
 urlpatterns = patterns('',
     # Examples:
     # url(r'^$', 'bennett_site.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', home)
 )
