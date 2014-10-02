8594541cac191d3bc8c4b3477495b411e0cf5198
Will Tachau <wtachau@gmail.com>
2014-10-02 14:29:11 -0700

    initial commit

---
+++ bennett/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/williamtachau/Development/freelance/bennett_project/bennett/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
