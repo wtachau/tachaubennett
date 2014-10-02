74cd9d64e25192bc3b72765b86c7424f98170153
Will Tachau <wtachau@gmail.com>
2014-10-02 16:49:46 -0700

    changing static file location

--- bennett_site/bennett_site/settings.py
+++ bennett_site/bennett_site/settings.py
@@ -82,4 +82,4 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = 'http://www.google.com/'
